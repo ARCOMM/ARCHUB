168284ff8054e23eea1415eb257a42f6e948ad47
TomBurch <thomas.burch1306@gmail.com>
2021-09-27 21:17:39 +0100

    Seed JoinStatus

---
+++ database/seeders/JoinStatusSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class JoinStatusSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('join_statuses')->insert([
+            'text' => 'Pending',
+            'permalink' => 'pending',
+            'position' => 0,
+            'protected' => true
+        ]);
+
+        DB::table('join_statuses')->insert([
+            'text' => 'Approved',
+            'permalink' => 'approved',
+            'position' => 1,
+            'protected' => true
+        ]);
+
+        DB::table('join_statuses')->insert([
+            'text' => 'Declined',
+            'permalink' => 'declined',
+            'position' => 2,
+            'protected' => true
+        ]);
+
+        DB::table('join_statuses')->insert([
+            'text' => 'Blacklisted',
+            'permalink' => 'blacklisted',
+            'position' => 3,
+            'protected' => true
+        ]);
+
+        DB::table('join_statuses')->insert([
+            'text' => 'Outdated',
+            'permalink' => 'outdated',
+            'position' => 4,
+            'protected' => false
+        ]);
+    }
+}
