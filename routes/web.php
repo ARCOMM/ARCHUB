9a63a898e4a13ec3ddfe2ebc89d2ed522284f6c8
TomBurch <thomas.burch1306@gmail.com>
2022-01-25 03:58:28 +0000

    Fix download endpoint

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,6 @@ Route::resource('join', 'PublicJoinController', [
 ]);
 
 Route::group(['middleware' => 'can:view-applications'], function () {
-    // Route::get('/hub/applications/transfer', 'JoinController@transferOldRecords');
     Route::get('/hub/applications/api/items', 'Join\JoinController@items');
     Route::get('/hub/applications/show/{jr}', 'Join\JoinController@show');
 
@@ -64,42 +63,35 @@ Route::group(['middleware' => 'can:access-hub'], function () {
         Route::post('/{operation}/missions', 'Operations\OperationMissionController@store');
         Route::delete('/{operation}/missions', 'Operations\OperationMissionController@destroy');
     });
-    
-    // Mission Media
-    Route::post('/hub/missions/media/add-photo', 'Missions\MediaController@uploadPhoto');
-    Route::post('/hub/missions/media/delete-photo', 'Missions\MediaController@deletePhoto');
-    Route::post('/hub/missions/media/add-video', 'Missions\MediaController@addVideo');
-    Route::post('/hub/missions/media/delete-video', 'Missions\MediaController@removeVideo');
 
     Route::prefix('hub/missions')->group(function() {
         Route::get('/{mission}/comments', 'Missions\CommentController@index');
         Route::post('/{mission}/comments', 'Missions\CommentController@store');
         Route::delete('/comments/{comment}', 'Missions\CommentController@destroy');
         Route::get('/comments/{comment}/edit', 'Missions\CommentController@edit');
-    });
 
-    Route::prefix('hub/missions')->group(function() {
         Route::get('/{mission}/notes', 'Missions\NoteController@index');
         Route::post('/{mission}/notes', 'Missions\NoteController@store');
         Route::delete('/notes/{note}', 'Missions\NoteController@destroy');
-    });
 
-    Route::prefix('hub/missions')->group(function() {
         Route::get('/{mission}/briefing/{faction}', 'Missions\BriefingController@index');
         Route::put('/{mission}/briefing/{faction}/lock', 'Missions\BriefingController@setLock');
+
+        Route::get('/{mission}/orbat/{faction}', 'Missions\MissionController@orbat');
     });
+
+    Route::post('/hub/missions/media/add-photo', 'Missions\MediaController@uploadPhoto');
+    Route::post('/hub/missions/media/delete-photo', 'Missions\MediaController@deletePhoto');
+    Route::post('/hub/missions/media/add-video', 'Missions\MediaController@addVideo');
+    Route::post('/hub/missions/media/delete-video', 'Missions\MediaController@removeVideo');
 
     // Missions
     Route::get('/hub/missions/{mission}/delete', 'Missions\MissionController@destroy');
     Route::post('/hub/missions/{mission}/update', 'Missions\MissionController@update');
     Route::post('/hub/missions/{mission}/set-verification', 'Missions\MissionController@updateVerification');
 
-    Route::prefix('hub/missions')->group(function() {
-        Route::get('/{mission}/orbat/{faction}', 'Missions\MissionController@orbat');
-    });
-
     // Download
-    Route::get('/hub/missions/{mission}/download/{format}', 'Missions\MissionController@download');
+    Route::get('/hub/missions/{mission}/download', 'Missions\MissionController@download');
 
     // Panels
     Route::get('/hub/missions/{mission}/{panel}', 'Missions\MissionController@panel');
